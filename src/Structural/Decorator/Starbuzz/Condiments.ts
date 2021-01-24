0bc596729a4567e4ca1e84242f8bf4268d8740d7
Nathan Brooks <nathan@3c22fbbbd4b1.ant.amazon.com>
2021-01-23 21:27:51 -0800

    remove export

--- src/Structural/Decorator/Starbuzz/Condiments.ts
+++ src/Structural/Decorator/Starbuzz/Condiments.ts
@@ -1,6 +1,6 @@
 import Beverage from "./Beverage";
 
-export abstract class CondimentDecorator extends Beverage {
+abstract class CondimentDecorator extends Beverage {
   protected _beverage: Beverage;
 
   constructor(beverage: Beverage) {
